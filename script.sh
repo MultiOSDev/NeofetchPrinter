0527c360569784ff0a773b451548ff497528ce28
MultiOSDev <81823905+MultiOSDev@users.noreply.github.com>
2023-04-04 14:08:14 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 # Aploigies for the black and while ony output, im working on a colour solution, but now its going to be available for all!
 # just letting you know, your gonna need pandoc, for now.
 neofetch > print.txt
-pandoc print.txt -o print.pdf
+enscript print.txt -o - | ps2pdf - print.pdf
 lp -o 3 print.pdf
